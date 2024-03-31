28709b1fe96b2a211b22d2df9e763236e30b4356
“Rubens <rubensmatheusvenancio@gmail.com>
2024-03-30 21:08:57 -0300

    created project

---
+++ hootel/src/main/java/com/grupo11/hootel/HootelApplication.java
@@ -0,0 +1,13 @@
+package com.grupo11.hootel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HootelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HootelApplication.class, args);
+	}
+
+}
---
+++ hootel/src/test/java/com/grupo11/hootel/HootelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.hootel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HootelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
