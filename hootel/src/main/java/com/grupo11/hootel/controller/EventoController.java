70c7fb576ef4767c683b277c6780549897ce327b
“Rubens <rubensmatheusvenancio@gmail.com>
2024-05-07 17:35:27 -0300

    add front e back inicial de eventos

--- hootel/src/main/java/com/grupo11/hootel/controller/EventoController.java
+++ hootel/src/main/java/com/grupo11/hootel/controller/EventoController.java
@@ -1,8 +1,14 @@
 package com.grupo11.hootel.controller;
 
+import com.grupo11.hootel.entity.Cardapio;
+import com.grupo11.hootel.entity.Evento;
 import com.grupo11.hootel.service.EventoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class EventoController {
@@ -13,7 +19,27 @@ public class EventoController {
     }
 
     @GetMapping("/eventos")
-    public String getEventos() {
+    public String getEventos(Model model) {
+        try {
+            List<Evento> eventos = eventoService.lerTodosEventos();
+            model.addAttribute("eventos", eventos);
+        }catch (Exception e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "cardapio-error";
+        }
         return "eventos";
     }
+
+    @GetMapping("/evento")
+    public String getEventoID(@RequestParam("eventoId") int theId,
+                              Model model) {
+        try {
+            Evento evento = eventoService.lerEventoId(theId);
+            model.addAttribute("evento_escolhido", evento);
+        }catch (Exception e){
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+        return "evento_especifico";
+    }
+
 }
